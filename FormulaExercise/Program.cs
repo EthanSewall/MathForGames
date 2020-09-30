cb364d8e896a6163379243e1e54c83025abdd458
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-09-30 16:51:25 -0700

    Initial Commit

---
+++ FormulaExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(QuadraticFunction(6).ToString());
+            Console.ReadKey();
+        }
+
+        static float QuadraticFunction(float x)
+        {
+            x = (x * x) + (x * 2) + (-7);
+
+            return x;
+        }
+    }
+}
