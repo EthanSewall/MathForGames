2adac96320c36c661e70ab0fc7c9bd3140076edb
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-09-30 17:35:19 -0700

    quadratic formula

--- FormulaExercise/Program.cs
+++ FormulaExercise/Program.cs
@@ -10,7 +10,8 @@ namespace FormulaExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(QuadraticFunction(6).ToString());
+            Console.WriteLine(QuadraticFormula(1,-1,-1)[0]);
+            Console.WriteLine(QuadraticFormula(1,-1,-1)[1]);
             Console.ReadKey();
         }
 
@@ -21,9 +22,21 @@ namespace FormulaExercise
             return x;
         }
 
-        static float[] QuadraticFormula(float a, float b)
+        static float[] QuadraticFormula(float a, float b, float c)
         {
-            return new float[] {a, b}; 
+            float root1 = 0;
+            float root2 = 0;
+            if ((b*b) - ( 4f * a * c)> 0)
+            {
+                root1 = ((b * -1f) + ((float)(Math.Sqrt((double)((b * b) - (4f * a * c)))))) /(2 * a);
+                root2 = ((b * -1f) - ((float)(Math.Sqrt((double)((b * b) - (4f * a * c)))))) /(2 * a);
+                return new float[] { root1, root2 };
+            }
+            else
+            {
+                Console.WriteLine("No roots.");
+                return null;
+            }
         }
     }
 }
