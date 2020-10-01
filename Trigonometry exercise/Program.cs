223546bd14720ed85348652b4b9f9c7083e9c49d
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-10-01 10:03:30 -0700

    radian degree conversion

---
+++ Trigonometry exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigonometry_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static float DegToRad(float x)
+        {
+            return (x/180) * (float)(Math.PI);
+        }
+        static float RadToDeg(float x)
+        {
+            return (x / (float)(Math.PI)) * 180;
+        }
+    }
+}
